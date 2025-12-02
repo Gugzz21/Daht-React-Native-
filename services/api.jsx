a2fa9dfe64c4c883b3f4b4ad336f1021fc7e754c
Gugzz21 <gustavodinizz966@gmail.com>
2025-12-02 14:08:17 -0300

    s

--- services/api.jsx
+++ services/api.jsx
@@ -1,25 +1,26 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+  import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
-// IP da sua rede Senac
-const API_URL = 'http://192.168.56.1:8412/api';
+  // IP da sua rede Senac
+// Este é o seu servidor, onde você tem controle e deve configurar o CORS
+const API_URL = 'http://10.12.10.13:8412/api';// Exemplo, o '/api' pode mudar.
 
-const api = axios.create({
-  baseURL: API_URL,
-});
+  const api = axios.create({
+    baseURL: API_URL,
+  });
 
-// INTERCEPTOR MÁGICO: 
-// Antes de qualquer requisição, ele pega o token e cola no cabeçalho.
-api.interceptors.request.use(async (config) => {
-  try {
-    const token = await AsyncStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+  // INTERCEPTOR MÁGICO: 
+  // Antes de qualquer requisição, ele pega o token e cola no cabeçalho.
+  api.interceptors.request.use(async (config) => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.log("Erro ao pegar token", error);
     }
-  } catch (error) {
-    console.log("Erro ao pegar token", error);
-  }
-  return config;
-});
+    return config;
+  });
 
-export default api;
+  export default api;
